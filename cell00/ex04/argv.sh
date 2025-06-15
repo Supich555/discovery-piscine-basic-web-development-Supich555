57b4d009f4364498dc581665e02d2f82560977e8
Supich555 <ssse4764@gmail.com>
2025-06-15 15:03:25 +0700

    argv.sh

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check the number of arguments
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+elif [ $# -eq 1 ]; then
+    echo "$1"
+elif [ $# -eq 2 ]; then
+    echo "$1"
+    echo "$2"
+elif [ $# -eq 3 ]; then
+    echo "$1"
+    echo "$2"
+    echo "$3"
+elif [ $# -gt 3 ]; then
+    echo "$1"
+    echo "$2"
+    echo "$3"
+fi
